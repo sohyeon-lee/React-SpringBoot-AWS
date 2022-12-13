b20c641eb91f4af61c59ee5206960c2086c362b7
isohyeon <isohyeon0816@gmail.com>
2022-12-14 01:40:14 +0900

    feat: todo 목록 조회 및 수정 기능 완료

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,27 +25,28 @@ public class TodoController {
 	@Autowired
 	private TodoService service;
 	
+	private static String temporaryUserId = "temporary-user"; // 임시 user id
+	
 	@GetMapping("/test")
 	public ResponseEntity<?> testTodo() {
 		String str = service.testService();
 		List<String> list = new ArrayList<>();
 		list.add(str);
-		
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		
 		return ResponseEntity.ok().body(response);
 	}
 	
 	@PostMapping
 	public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
 		try {
-			String temporaryUserId = "temporary-user"; // temporary user id.
-
 			// TodoEntity로 변환한다.
 			TodoEntity entity = TodoDTO.toEntity(dto);
 			// id를 null로 초기화 한다. 생성 당시에는 id가 없어야 하기 때문이다.
 			entity.setId(null);
 			// 인증과 인가에서 수정 할 예정, 지금은 인증과 인가 기능이 없으므로 한 유저(temporary-user)만 로그인 없이 사용 가능
 			entity.setUserId(temporaryUserId);
+			
 			// 서비스를 이용해 Todo엔티티를 생성한다.
 			List<TodoEntity> entities = service.create(entity);
 			
@@ -58,16 +60,58 @@ public class TodoController {
 				todoDTO.setDone(entities.get(i).isDone());
 				dtos.add(todoDTO);
 			}
-			
 			// 변환된 TodoDTO리스트를 이용해 ResponseDTO를 초기화한다.
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
 			return ResponseEntity.ok().body(response);
 			
 		} catch (Exception e) {
 			// 혹시 예외가 나는 경우 dto대신 error에 메시지를 넣어 리턴한다.
 			String error = e.getMessage();
 			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			
 			return ResponseEntity.badRequest().body(response);
 		}
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> retrieveTodoList() {
+		// 서비스 클래스의 retrieve 메서드를 사용해 Todo 리스트를 가져온다.
+		List<TodoEntity> entities = service.retrieve(temporaryUserId);
+		
+		// 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO리스트로 변환
+		//List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+		List<TodoDTO> dtos = new ArrayList<>();
+		TodoDTO todoDTO = new TodoDTO();
+		for(int i = 0; i < entities.size(); i++) {
+			todoDTO.setId(entities.get(i).getId());
+			todoDTO.setTitle(entities.get(i).getTitle());
+			todoDTO.setDone(entities.get(i).isDone());
+			dtos.add(todoDTO);
+		}
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+		
+		return ResponseEntity.ok().body(response);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> updateTodo(@RequestBody TodoDTO dto) {
+		TodoEntity entity = TodoDTO.toEntity(dto);
+		entity.setUserId(temporaryUserId); // 수정 예정
+		
+		List<TodoEntity> entities = service.update(entity);
+		
+		//List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+		List<TodoDTO> dtos = new ArrayList<>();
+		TodoDTO todoDTO = new TodoDTO();
+		for(int i = 0; i < entities.size(); i++) {
+			todoDTO.setId(entities.get(i).getId());
+			todoDTO.setTitle(entities.get(i).getTitle());
+			todoDTO.setDone(entities.get(i).isDone());
+			dtos.add(todoDTO);
+		}
+		ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+		
+		return ResponseEntity.ok().body(response);
 	}
 }
--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,10 @@ import com.example.demo.persistence.TodoRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * @author isohy
+ *
+ */
 @Slf4j
 @Service
 public class TodoService {
@@ -17,18 +22,21 @@ public class TodoService {
 	@Autowired
 	private TodoRepository repository;
 	
+	/**
+	 * @return
+	 */
 	public String testService() {
-		
-		// TodoEntity 생성
 		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-		// TodoEntity 저장
 		repository.save(entity);
-		// TodoEntity 검색
 		TodoEntity savedEntity = repository.findById(entity.getId()).get(); // findById JPA 제공 메서드
 		
 		return savedEntity.getTitle();
 	}
 	
+	/**
+	 * @param entity
+	 * @return
+	 */
 	public List<TodoEntity> create(final TodoEntity entity) {
 		validate(entity); 
 
@@ -37,6 +45,7 @@ public class TodoService {
 		return repository.findByUserId(entity.getUserId());
 	}
 	
+	// 데이터 검증 메서드
 	// 넘어온 데이터가 유효한지 검사, 코드가 더 커지면 클래스로 분리 예정
 	private void validate(final TodoEntity entity) {
 		if(entity == null) {
@@ -48,5 +57,38 @@ public class TodoService {
 			log.warn("Unknown user.");
 			throw new RuntimeException("Unknown user.");
 		}
+	}
+	
+	/**
+	 * @param userId
+	 * @return
+	 */
+	public List<TodoEntity> retrieve(final String userId) {
+		return repository.findByUserId(userId);
+	}
+	
+	/**
+	 * @param entity
+	 * @return
+	 */
+	public List<TodoEntity> update(final TodoEntity entity) {
+		validate(entity);
+		
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+//		original.ifPresent(todo -> {
+//			todo.setTitle(entity.getTitle());
+//			todo.setDone(entity.isDone());
+//			
+//			repository.save(todo);
+//		});
+		if(original.isPresent()) {
+			final TodoEntity todo = original.get();
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			
+			repository.save(todo);
+		}
+		
+		return retrieve(entity.getUserId());
 	}
 }
