8e203b0d0248b98e042b2b925a0fcd7d1fd93ea7
isohyeon <isohyeon0816@gmail.com>
2022-12-19 01:54:03 +0900

    feat: CORS 설정을 위한 클래스 추가
    frontend와 서비스 통합을 위해 CORS를 가능하게 하기 위한 설정을 추가함

---
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS 설정을 위한 클래스
+ * 
+ * @author isohyeon
+ *
+ */
+@Configuration // 스프링 빈으로 등록
+public class WebMvcConfig implements WebMvcConfigurer {
+	private final long MAX_AGE_SECS = 3600;
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**") // 모든 경로에 대해
+			.allowedOrigins("http://localhost:3000") // Origin이 http://localhost:3000인 경우
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드 요청을 허용한다.
+			.allowedHeaders("*") // 모든 헤더와
+			.allowCredentials(true) // 인증에 관한 정보도 허용 한다.
+			.maxAge(MAX_AGE_SECS);
+	}
+}
