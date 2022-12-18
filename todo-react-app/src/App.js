76536cadc9550857478aa6ec15842cdc95973ecf
isohyeon <isohyeon0816@gmail.com>
2022-12-18 16:55:58 +0900

    feat: todo 삭제  ui 및 기능 추가
    App.js - deleteItem() 함수 작성
    Todo.js - 삭제 버튼 추가 및 deleteItem() 기능 연결

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -6,27 +6,23 @@ import AddTodo from './AddTodo';
 import { List, Paper } from '@mui/material';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id:'0',
-      title:'Hello World 1',
-      done: true
-    },
-    {
-      id:'1',
-      title:'Hello World 2',
-      done: true
-    }
-  ]);
+  const [items, setItems] = useState([]);
 
+  // todo 추가
   const addItem = (item) => {
     item.id = "ID-" + items.length; // key를 위한 id
     item.done = false; // done 초기화
     setItems([...items, item]);  // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 한다.
     console.log("items : ", [...items, item]);
   }
+
+  // todo 삭제
+  const deleteItem = (item) => {
+    const newItems = items.filter(e => e.id !== item.id); // 삭제할 아이템
+    setItems([...newItems]); // 삭제할 아이템을 제외한 아이템을 다시 배열에 저장
+  }
   
-  let todoItems = items.length > 0 && items.map((item)=><Todo item={item} key={item.id}/>);
+  let todoItems = items.length > 0 && items.map((item)=><Todo item={item} key={item.id} deleteItem={deleteItem}/>);
   
   return (
     <div className="App container">
--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
-import { Checkbox, InputBase, ListItem, ListItemText } from '@mui/material';
+import { Checkbox, IconButton, InputBase, ListItem, ListItemSecondaryAction, ListItemText } from '@mui/material';
+import { DeleteOutlined } from '@mui/icons-material';
 
 function Todo(props) {
   const [item, setItem] = useState(props.item);
+  const deleteItem = props.deleteItem; // App에서 전달 받은 todo 삭제 함수
+
+  // deleteEventHandler 작성
+  const deleteEventHandler = () => {
+    deleteItem(item);
+  }
 
   return (
     <>
       <ListItem>
         <Checkbox checked={item.done} />
+        
         <ListItemText>
           <InputBase
             inputProps={{ 'aria-label': 'naked' }}
@@ -19,6 +27,12 @@ function Todo(props) {
             fullWidth={true}
           />
         </ListItemText>
+
+        <ListItemSecondaryAction>
+          <IconButton aria-label='Delete Todo' onClick={deleteEventHandler}>
+            <DeleteOutlined />
+          </IconButton>
+        </ListItemSecondaryAction>
       </ListItem>
 
       {/* reactstrap */}
