864f2831dd62421afbfdeed2fdded5616c852e3f
LAPTOP-6D3NOR16\Developer <isohyeon0816@gmail.com>
2022-12-28 20:26:36 +0900

    fix: 로그인 토큰 검증 실패 오류 해결

--- demo/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++ demo/src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
@@ -88,7 +88,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		String bearerToken = request.getHeader("Authorization");
 		
 		if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
-			return bearerToken.substring(7);
+			return bearerToken.substring(6);
 		}
 		return null;
 	}
