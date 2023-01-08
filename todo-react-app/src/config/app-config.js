18d4d99939af538a6889e83e7eb52f7672582099
isohyeon <isohyeon0816@gmail.com>
2023-01-09 01:59:30 +0900

    feat: 백엔드 엔드포인트로 경로 변경

---
+++ todo-react-app/src/config/app-config.js
@@ -0,0 +1,8 @@
+let backendHost;
+const hostname = window && window.location && window.location.hostname;
+
+if(hostname === 'localhost') {
+    backendHost = 'http://todoapplication.us-west-2.elasticbeanstalk.com';
+}
+
+export const API_BASE_URL = `${backendHost}`;
