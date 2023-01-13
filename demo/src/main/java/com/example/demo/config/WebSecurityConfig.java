0c3e5ef3bec1a8a3b2c48b5a8db872b82c7fcf25
LAPTOP-6D3NOR16\Developer <isohyeon0816@gmail.com>
2023-01-13 10:51:19 +0900

    feat: oauth2.0 엔드포인트 설정

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -46,12 +46,18 @@ public class WebSecurityConfig {
 		http
 			.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
 			.and()
-			.csrf().disable() // csrf는 현재 사용하지 않으므로 disable
-			.httpBasic().disable() // token을 사용하므로 basic 인증 disable
+			.csrf()
+			.disable() // csrf는 현재 사용하지 않으므로 disable
+			.httpBasic()
+			.disable() // token을 사용하므로 basic 인증 disable
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 기반이 아님
 			.and()
-			.authorizeRequests().antMatchers("/", "/auth/**").permitAll() // /와 /auth/** 경로는 인증 안해도 됨
-			.anyRequest().authenticated(); // 이외의 모든 경로는 인증해야 함
+			.authorizeRequests()
+			.antMatchers("/", "/auth/**").permitAll() // /와 /auth/** 경로는 인증 안해도 됨
+			.anyRequest()
+			.authenticated() // 이외의 모든 경로는 인증해야 함
+			.and()
+			.oauth2Login();
 		
 		// filter 등록
 		// 매 요청마다 CorsFilter 실행한 후에 jwtAuthenticationFilter 실행
