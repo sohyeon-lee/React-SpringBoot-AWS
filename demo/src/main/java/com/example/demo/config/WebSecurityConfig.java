099cb671ecdff3eeb746e6b8ed1f65d2ea04cfab
LAPTOP-6D3NOR16\Developer <isohyeon0816@gmail.com>
2023-01-13 11:00:10 +0900

    feat: oauth2, redirect 엔드포인트 추가

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -43,21 +43,22 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// http 시큐리티 빌더
-		http
-			.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
-			.and()
-			.csrf()
-			.disable() // csrf는 현재 사용하지 않으므로 disable
-			.httpBasic()
-			.disable() // token을 사용하므로 basic 인증 disable
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 기반이 아님
-			.and()
-			.authorizeRequests()
-			.antMatchers("/", "/auth/**").permitAll() // /와 /auth/** 경로는 인증 안해도 됨
-			.anyRequest()
-			.authenticated() // 이외의 모든 경로는 인증해야 함
-			.and()
-			.oauth2Login();
+		http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+				.and()
+				.csrf()
+				.disable() // csrf는 현재 사용하지 않으므로 disable
+				.httpBasic()
+				.disable() // token을 사용하므로 basic 인증 disable
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 기반이 아님
+				.and()
+				.authorizeRequests()
+				.antMatchers("/", "/auth/**", "/oauth2/**").permitAll() // /와 /auth/** 경로는 인증 안해도 됨
+				.anyRequest()
+				.authenticated() // 이외의 모든 경로는 인증해야 함
+				.and()
+				.oauth2Login()
+				.redirectionEndpoint()
+				.baseUri("/oauth2/callback/*");
 		
 		// filter 등록
 		// 매 요청마다 CorsFilter 실행한 후에 jwtAuthenticationFilter 실행
