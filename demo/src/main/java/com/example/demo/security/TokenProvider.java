f563afd469b1427af1fb51a5059b84abd1c38359
isohyeon <isohyeon0816@gmail.com>
2023-01-15 05:21:57 +0900

    feat: Authentication을 매개변수로 받는 create 메서드 오버로딩

--- demo/src/main/java/com/example/demo/security/TokenProvider.java
+++ demo/src/main/java/com/example/demo/security/TokenProvider.java
@@ -1,9 +1,11 @@
 package com.example.demo.security;
 
+import java.net.Authenticator;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.UserEntity;
@@ -15,26 +17,25 @@ import lombok.extern.slf4j.Slf4j;
 
 /**
  * JWT를 생성하고 검증하는 클래스
- * 
- * @author isohyeon
  *
+ * @author isohyeon
  */
 @Slf4j
 @Service
 public class TokenProvider {
 	private static final String SECRET_KEY = "1UQJ5XsXc8ZtKZdfqsJZmrVEyHH4jvoJpmzkEsIjqki4F9HwxWSuwnjuwkKxtcFcNKM9gLhAPm3TfhJxYEEfoNvUQlLvnnE5JrlQ";
-	
+
 	/**
 	 * JWT Token 생성 메서드
 	 * 유저 정보를 바탕으로 헤더와 페이로드를 작성하고 전자 서명한 후 토큰을 반환한다.
-	 * 
+	 *
 	 * @param userEntity JWT Token 생성을 위한 유저 정보를 담은 엔티티
 	 * @return JWT Token
 	 */
 	public String create(UserEntity userEntity) {
 		// 기한 지금으로부터 1일로 설정
 		Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
-		
+
 		// JWT Token 생성
 		return Jwts.builder()
 				// header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
@@ -46,10 +47,30 @@ public class TokenProvider {
 				.setExpiration(expiryDate) // exp
 				.compact();
 	}
-	
+
+	/**
+	 * OAuth 2.0 인증 시 JWT 토큰 생성
+	 *
+	 * @param authentication OAuth 인증 객체
+	 * @return JWT Token
+	 */
+	public String create(final Authentication authentication) {
+		ApplicationOAuth2User userPrincipal = (ApplicationOAuth2User) authentication.getPrincipal();
+		Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+		return Jwts.builder()
+				.signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+				// payload에 들어갈 내용
+				.setSubject(userPrincipal.getName()) // id가 리턴됨
+				.setIssuer("demo app") // iss
+				.setIssuedAt(new Date()) // iat
+				.setExpiration(expiryDate) // exp
+				.compact();
+	}
+
 	/**
 	 * JWT Token 검증(디코딩, 파싱 및 위조여부 확인) 메서드
-	 * 
+	 *
 	 * @param token 검증하려는 JWT Token
 	 * @return 검증된 userId(subject) 또는 예외
 	 */
@@ -62,7 +83,7 @@ public class TokenProvider {
 				.setSigningKey(SECRET_KEY)
 				.parseClaimsJws(token)
 				.getBody();
-		
+
 		return claims.getSubject();
 	}
 }
