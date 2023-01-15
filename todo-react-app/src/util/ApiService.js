f73d04e8f5bc4b9462753a44c3ea135e531ffd19
isohyeon <isohyeon0816@gmail.com>
2023-01-16 02:16:33 +0900

    feat: socialLogin 함수에 redirect_url 매개변수 추가
    백엔드가 토큰 생성 후 redirect 해야하는 url을 쿼리 파라미터로 전달함.

--- todo-react-app/src/util/ApiService.js
+++ todo-react-app/src/util/ApiService.js
@@ -94,5 +94,6 @@ export function signup(userDTO) {
  * @param {String} provider 
  */
 export function socialLogin(provider) {
-  window.location.href = API_BASE_URL + "/auth/authorize/" + provider;
+  const frontendUrl = window.location.protocol + "//" + window.location.host;
+  window.location.href = API_BASE_URL + "/auth/authorize/" + provider + "?redirect_url=" + frontendUrl;
 }
